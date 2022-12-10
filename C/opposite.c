60f7637d195e3873b37e9cec398b6062f5bf78f7
IanoNjuguna <inc3099@gmail.com>
2022-12-10 22:14:31 +0300

    Find the opposite of a number

---
+++ C/opposite.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * opposite - Finds the opposite of a number
+ *
+ * @num: Contains the value of the number
+ * Return: - num (Opposite of the number)
+ */
+
+float opposite(float num)
+{
+	return (-num);
+}
+
