07325ddb3f867e5b0c98246c501cf092e2f483bd
IanoNjuguna <inc3099@gmail.com>
2022-12-10 22:03:05 +0300

    Function changes speed from km/h to cm/h

---
+++ C/cockroach_speed.c
@@ -0,0 +1,18 @@
+#include <math.h>
+#include <stdio.h>
+
+/**
+ * cockroach_speed - Takes speed in km/h and returns it in cm/h, floored.
+ *
+ * @s: Speed in km/h
+ * @speed_cmh: Speed in cm/h
+ * Return: speed_cmh (Success)
+ */
+
+int cockroach_speed(double s)
+{
+	int speed_cmh = floor(s * 27.7778);
+
+	return (speed_cmh);
+}
+
