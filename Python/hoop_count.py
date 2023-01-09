0408326e2e5bdbd14e30cf57f0fe930355442052
IanoNjuguna <inc3099@gmail.com>
2023-01-09 13:00:42 +0300

    hoop count

---
+++ Python/hoop_count.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+
+# Counts number of times hulahoop goes round
+
+def hoop_count(n):
+    if (n >= 10):
+        return ("Great, now move on to tricks")
+    else:
+        return ("Keep at it until you get it")
